e6d51bba7eeb322333896972a0e119270817ca6e
GardRyan <res.gardner@gmail.com>
2024-03-03 20:00:15 -0500

    added a benchmark function

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,10 @@
 def benchmark
   # Your benchmarking code goes here.
+  sum_of_numbers = 30+20+10+1
+
+  running_time = benchmark {sum_of_numbers}
+
+  puts "This only took #{running_time} second to run"
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
